6d9332d39f01161b6302edef28e85a0daada6a16
romansaransk <59063087+romansaransk@users.noreply.github.com>
2020-01-24 23:55:53 +0300

    Add files via upload

---
+++ лаба10/src/main/java/task10.java
@@ -0,0 +1,116 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+class UserHandler extends Thread{
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Date debounce;
+
+    private String calc(String input){
+        String[] splitedinput = input.split(" ");
+        if (splitedinput.length != 3){
+            return "Invalid input";
+        }
+        if (splitedinput[1].equals("add")){
+            try{
+                float one = Float.parseFloat(splitedinput[0]);
+                float two = Float.parseFloat(splitedinput[2]);
+                return Float.toString(one+two);
+            } catch (Exception t){
+                return "Invalid input";
+            }
+        } else if (splitedinput[1].equals("mult")){
+            try{
+                float one = Float.parseFloat(splitedinput[0]);
+                float two = Float.parseFloat(splitedinput[2]);
+                return Float.toString(one*two);
+            } catch (Exception t){
+                return "Invalid input";
+            }
+
+        }else if (splitedinput[1].equals("subs")){
+            try{
+                float one = Float.parseFloat(splitedinput[0]);
+                float two = Float.parseFloat(splitedinput[2]);
+                return Float.toString(one-two);
+            } catch (Exception t){
+                return "Invalid input";
+            }
+
+        }else if (splitedinput[1].equals("divide")){
+            try{
+                float one = Float.parseFloat(splitedinput[0]);
+                float two = Float.parseFloat(splitedinput[2]);
+                return Float.toString(one/two);
+            } catch (Exception t){
+                return "Invalid input";
+            }
+
+        } else{
+            return "Unsupported operation";
+        }
+    }
+
+//
+
+    public UserHandler (Socket socket) throws IOException{
+        this.socket = socket;
+        out = new PrintWriter(socket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        debounce = new Date();
+        start();
+    }
+    @Override
+    public void run() {
+        String line;
+        try {
+            while ((line = in.readLine()) != null && (new Date().getTime() - debounce.getTime()) < 60000) {
+
+                String output = calc(line);
+                out.println("Your result: " + output);
+                debounce = new Date();
+                }
+            out.println("There's no request during long period of time. Good Bye!");
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+public class task10 {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerSocket server = new ServerSocket(8888);
+        try {
+                System.out.println("Server has started on 127.0.0.1:8888.\r\nWaiting for a connection...");
+            while(true) {
+                try {
+                    System.out.println("Waiting for the client request");
+                    Socket socket = server.accept();
+                    try {
+                        new UserHandler(socket);
+                    } catch (IOException e){
+                        socket.close();
+                    }
+
+                } catch (IOException e){
+                    server.close();
+                    System.out.println("Error on accept socket!");
+                    break;
+                }
+            }
+
+        } catch (Exception t){
+            System.out.println("Exception : " + t);
+        }
+
+
+    }
+}
